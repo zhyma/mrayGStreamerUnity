3da2e92c1fd303eab2e0ee8330ec612737a0de2a
Zhaoyuan Ma <zma3@wpi.edu>
2019-05-28 16:37:22 -0400

    The screen is pinned to the front of the view. Need to solve the jittering.

---
+++ Unity/UnityTests/Assets/Scenes/Scripts/ScreenMotion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenMotion : MonoBehaviour
+{
+
+    public Transform targetCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(targetCamera.position);
+        transform.position = targetCamera.position + targetCamera.forward * 2;
+        transform.rotation = Quaternion.LookRotation(targetCamera.position-transform.position)*Quaternion.Euler(0, 180, 0);
+        Debug.Log(targetCamera.forward);
+        //Debug.Log(targetCamera.position);
+    }
+}
